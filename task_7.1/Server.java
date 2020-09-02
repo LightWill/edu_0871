5c3bd9e03d11e3111c1fca91649ec411eefb03b5
AlexLight <LightWill@users.noreply.github.com>
2020-09-02 16:08:38 +0300

    Update Server.java

--- task_7.1/Server.java
+++ task_7.1/Server.java
@@ -1,84 +1,37 @@
-package Server;
+package Client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Server {
-    static ArrayList<User> users = new ArrayList<>();
-
+public class Client {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(8189);
-            System.out.println("Сервер запущен");
-            while (true) {
-                Socket socket = serverSocket.accept();
-                DataInputStream incoming = new DataInputStream(socket.getInputStream());
-                DataOutputStream outgoing = new DataOutputStream(socket.getOutputStream());
-                System.out.println("Клиент подключен.");
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
+            Socket socket = new Socket("localhost", 8189);
+            DataInputStream incoming = new DataInputStream(socket.getInputStream());
+            DataOutputStream outgoing = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            Thread responseThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        String response = null;
                         try {
-                            outgoing.writeUTF("Укажите ваше имя:");
-                            String name = incoming.readUTF();
-                            System.out.println("name: " + name);
-
-                            User user = new User(name, socket);
-                            users.add(user);
-
-                            while (true) {
-                                String str = incoming.readUTF();
-                                broadcastMsg(str);
-                                System.out.println("Клиент прислал сообщение2: " + str);
-                            }
+                            response = incoming.readUTF();
+                            System.out.println(response);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
-                });
-                thread.start();
+                }
+            });
+            responseThread.start();
+            while (true) {
+                String str = scanner.nextLine();
+                outgoing.writeUTF(str);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void broadcastMsg(String str) {
-        DataOutputStream outgoing;
-        for (User user: users) {
-            user.send(str);
-        }
-    }
-
-    public static void sendToClient(String name, String text) {
-        for (User user: users) {
-            if (user.name.equals(name)) {
-                user.send(text);
-                break;
-            }
-        }
-    }
-
-}
-
-class User {
-    String name;
-    Socket socket;
-
-    User(String name, Socket socket) {
-        this.name = name;
-        this.socket = socket;
-    }
-
-    void send(String text) {
-        DataOutputStream outgoing;
-        try {
-            outgoing = new DataOutputStream(socket.getOutputStream());
-            outgoing.writeUTF(text);
         } catch (IOException e) {
             e.printStackTrace();
         }
